9a07e2707bef50beb03eff014c596338b6df38ee
adibfirman <dib.firman@gmail.com>
2018-06-22 18:36:07 +0700

    learing object literals

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('testing')
+var userOne = {
+  username: 'adib',
+  fullname: 'adib firman',
+  login() {
+    console.log(this.username, 'has been login')
+  },
+  logout() {
+    console.log(this.username, 'has been logout')
+  }
+}
+
+console.log(userOne.username)
