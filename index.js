e204dd6c88398af4950b585f5f86138184bf3663
adibfirman <dib.firman@gmail.com>
2018-06-23 13:06:09 +0700

    learning method chaining in class

--- index.js
+++ index.js
@@ -2,17 +2,29 @@ class User {
   constructor(email, password) { // passing parameter class with function `constructor`
     this.email = email
     this.password = password
+    this.score = 0
   }
 
   login () {
     console.log(this.email, 'has been login')
+
+    return this
   }
 
   logout () {
     console.log(this.email, 'has been logout')
+
+    return this
+  }
+
+  updatedScore () {
+    this.score++
+    console.log(this.email, 'have score is', this.score)
+
+    return this
   }
 }
 
 const userOne = new User('adibfirman@test.com', 'testing123')
 
-console.log(userOne.login())
+userOne.login().updatedScore().updatedScore().updatedScore().logout()
