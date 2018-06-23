9da7ab3b95354965fd340ad249bbff540867ec31
adibfirman <dib.firman@gmail.com>
2018-06-23 12:57:39 +0700

    learning basic class

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-var userOne = {
-  username: 'adib',
-  fullname: 'adib firman',
-  login() {
-    console.log(this.username, 'has been login')
-  },
-  logout() {
-    console.log(this.username, 'has been logout')
+class User {
+  constructor(email, password) { // passing parameter class with function `constructor`
+    this.email = email
+    this.password = password
+  }
+
+  login () {
+    console.log(this.email, 'has been login')
+  }
+
+  logout () {
+    console.log(this.email, 'has been logout')
   }
 }
 
-console.log(userOne.username)
+const userOne = new User('adibfirman@test.com', 'testing123')
+
+console.log(userOne.login())
