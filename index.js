481484a3e4bdf5475d7fc5b4dd489529c694335c
adibfirman <dib.firman@gmail.com>
2018-06-23 14:54:53 +0700

    learning prototype

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ function User(email, password) {
   this.email = email
   this.password = password
   this.online = false
-  this.login = function () {
-    this.online = true
+}
 
-    console.log(this.email, 'just login in')
-  }
+User.prototype.login = function () {
+  this.online = true
+  console.log(this.email, 'has logged in')
+}
+
+User.prototype.logout = function () {
+  this.online = false
+  console.log(this.email, 'has logged out')
 }
 
 const userOne = new User('adib@dib.com', 'dib123')
